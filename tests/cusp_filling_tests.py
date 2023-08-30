2025dca1dae836c8fc7d6102c501184f52588d9a
jmalionek <jmalionek@gmail.com>
2023-08-30 16:29:54 -0500

    Added slurm files for links and cusps (currently only running on manifolds with trivial alex poly)

---
+++ tests/cusp_filling_tests.py
@@ -0,0 +1,92 @@
+#! /data/keeling/a/nmd/miniconda3/envs/sage_full/bin/sage-python -u
+#
+#SBATCH --partition m
+#SBATCH --tasks=1
+#SBATCH --mem-per-cpu=4G
+#SBATCH --nice=10000
+#SBATCH --time=7-00:00
+#SBATCH --array=0-19
+#SBATCH --output=/data/keeling/a/jdm7/filled_cusps/slurm_out_%A_%a
+#SBATCH --error=/data/keeling/a/jdm7/slurm_error/filled_cusps_%a
+import time
+
+import snappy
+import sys, os
+from itertools import product
+
+from sage.all import gcd, FreeModule, matrix, ZZ, vector
+from snappy.snap.polished_reps import MapToFreeAbelianization
+sys.path.append('./..')
+import twistable_revamp
+import pickle
+import pandas as pd
+import interpolate
+import norm_in_closed
+
+
+
+def main():
+	census = snappy.OrientableCuspedCensus()
+
+	num_jobs = 20
+	task = int(os.environ['SLURM_ARRAY_TASK_ID'])
+
+	num_manifolds = len(census)
+
+	for i in range(task, num_manifolds, num_jobs):
+
+		M = census[i]
+		if M.solution_type(enum = True) > 1:
+			continue
+		G =  M.fundamental_group()
+		Ab = MapToFreeAbelianization(G)
+		if M.num_cusps() > 2:
+			continue
+		fillings = []
+		if M.num_cusps() == 1:
+			fillings.append(M.homological_longitude())
+		if M.num_cusps() == 2:
+			a1 = Ab(G.meridian(0))
+			b1 = Ab(G.longitude(0))
+			a2 = Ab(G.meridian(1))
+			b2 = Ab(G.longitude(1))
+			A1 = matrix(ZZ, [a1, b1])
+			A2 = matrix(ZZ, [a2, b2])
+			try:
+				short_slopes = M.short_slopes(12)
+			except:
+				continue
+			for s1 in short_slopes[0]:
+				s1 = vector(ZZ, s1)
+				for s2 in short_slopes[1]:
+					s2 = vector(ZZ, s2)
+					if s1*A1 == s2*A2:
+						fillings.append([s1, s2])
+
+		for filling in fillings:
+			M.dehn_fill(filling)
+			assert M.homology().betti_number() > 0
+			name = M.name()
+			if f'{name}_output' in os.listdir():
+				continue
+			M = M.high_precision()
+			if M.volume() < .5 or M.solution_type(enum = True) > 2:
+				continue
+			if M.homology().betti_number() != 1:
+				continue
+			if M.alexander_polynomial().degree() >= 1:
+				continue
+			tic = time.perf_counter()
+			upper = norm_in_closed.search_for_small_norm_surface(M)
+			poly = approximate_polynomial(M, tol = .00000001, method='golden_angle')
+			elapsed = time.perf_counter() - tic
+			out = {'poly': poly, 'time':elapsed, 'alex': M.alexander_polynomial(), 'upper_bound':upper}
+			coeffs = poly.coefficients()
+			for j in range(len(coeffs)):
+				if (coeffs[j] - coeffs[-j-1]).abs() > .0001:
+					print(f'{j} coefficient of manifold {name} not symmetric')
+			with open(f'/data/keeling/a/jdm7/filled_cusps/{name}_output', 'wb') as file:
+				pickle.dump(out, file)
+
+if __name__ == '__main__':
+	main()
---
+++ tests/link_filling_tests.py
@@ -0,0 +1,78 @@
+#! /data/keeling/a/nmd/miniconda3/envs/sage_full/bin/sage-python -u
+#
+#SBATCH --partition m
+#SBATCH --tasks=1
+#SBATCH --mem-per-cpu=4G
+#SBATCH --nice=10000
+#SBATCH --time=7-00:00
+#SBATCH --array=0-19
+#SBATCH --output=/data/keeling/a/jdm7/filled_links/slurm_out_%A_%a
+#SBATCH --error=/data/keeling/a/jdm7/slurm_error/filled_links_%a
+import time
+
+import snappy
+import sys, os
+from itertools import product
+
+from sage.all import gcd
+sys.path.append('./..')
+import twistable_revamp
+import pickle
+import pandas as pd
+import interpolate
+
+
+
+def main():
+	census = snappy.HTLinkExteriors()
+
+	num_jobs = 20
+	task = int(os.environ['SLURM_ARRAY_TASK_ID'])
+	num_links = len(census)
+
+	for i in range(task, num_links, num_jobs):
+		M = census[i]
+		if M.solution_type(enum = True) > 1:
+			continue
+		if M.num_cusps() > 2:
+			continue
+		fillings = []
+		if M.num_cusps == 1:
+			fillings.append(M.homological_longitude())
+		if M.num_cusps == 2:
+			n = M.link().linking_number()
+			try:
+				short_slopes = M.short_slopes(12)
+			except:
+				continue
+			for a1, b1 in short_slopes[0]:
+				for a2, b2 in short_slopes[1]:
+					if a1*a2 == b1*b2*n**2 and gcd(a1, b1) == gcd(a2, b2) == 1:
+						fillings.append([(a1, b1), (a2, b2)])
+
+		for filling in fillings:
+			M.dehn_fill(filling)
+			name = M.name()
+			if f'{name}_output' in os.listdir():
+				continue
+			M = M.high_precision()
+			if M.volume() < .5 or M.solution_type(enum = True) > 3:
+				continue
+			if M.homology().betti_number() != 1:
+				continue
+			tic = time.perf_counter()
+			if M.alexander_polynomial().degree() >= 2:
+				continue
+			upper = norm_in_closed.search_for_small_norm_surface(M)
+			poly = approximate_polynomial(M, tol = .00000001, method='golden_angle')
+			elapsed = time.perf_counter() - tic
+			out = {'poly': poly, 'time': elapsed, 'alex': M.alexander_polynomial(), 'upper_bound': upper}
+			coeffs = poly.coefficients()
+			for j in range(len(coeffs)):
+				if (coeffs[j] - coeffs[-j-1]).abs() > .0001:
+					print(f'{j} coefficient of manifold {name} not symmetric')
+			with open(f'/data/keeling/a/jdm7/filled_links/{name}_output', 'wb') as file:
+				pickle.dump(out, file)
+
+if __name__ == '__main__':
+	main()
